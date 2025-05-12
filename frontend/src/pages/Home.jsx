b5dcc631fa2c20498d8f2e7216e2841938dff859
Anushka <anushka031205@gmail.com>
2025-05-12 11:12:34 +0530

    day 2

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,54 +5,73 @@ const Home = () => {
   return (
     <div className="bg-gray-100 bg-cover w-screen h-screen font-visby flex">
       {/* Navbar section */}
-      <div className="absolute top-0 left-0 w-full p-10 flex justify-between items-center z-10">
+      <div className="absolute top-0 left-0 w-full p-10 flex items-center z-10">
 
-        {/* Left side: Logo + Nav */}
-        <div className="flex items-center space-x-10">
-          <div className="text-xl font-bold">Logo</div>
-          <ul className="flex space-x-8 text-gray-600">
-            <li className="cursor-pointer hover:text-black">About</li>
-            <li className="cursor-pointer hover:text-black">Projects</li>
-            <li className="cursor-pointer hover:text-black">Contact</li>
-          </ul>
+        <div className="text-xl font-bold">Logo</div>
+
+        {/* Center: Nav */}
+        <div className="absolute left-1/2 transform -translate-x-1/2">
+          <div className="border-gray-700 rounded-3xl p-2 px-4 border-2 border-solid">
+            <ul className="flex space-x-8 text-gray-600">
+              <li className="cursor-pointer hover:text-black">About</li>
+              <li className="cursor-pointer hover:text-black">Projects</li>
+              <li className="cursor-pointer hover:text-black">Contact</li>
+            </ul>
+          </div>
         </div>
 
-        {/* Right side: Socials */}
-        <ul className="flex space-x-8 mr-10 text-gray-600">
-          <li className="cursor-pointer hover:text-black">LinkedIn</li>
-          <li className="cursor-pointer hover:text-black">GitHub</li>
-        </ul>
-
+        {/* Right: Socials */}
+        <div className="flex-1 flex justify-end">
+          <div className="flex justify-center items-center border-gray-700 rounded-3xl p-2 px-4 border-2 border-solid">
+            <ul className="flex space-x-8 text-gray-600">
+              <li className="cursor-pointer hover:text-black">LinkedIn</li>
+              <li className="cursor-pointer hover:text-black">GitHub</li>
+            </ul>
+          </div>
+        </div>
       </div>
+
 
       {/* Main Content Section */}
       <div>
-        {/* type writer */}
-        <div className='text-[120px] font-bold m-10 mt-52'>
-          <Typewriter
-            options={{
-              loop: true,
-              delay: 75,
-              html: true,
-            }}
-            onInit={(typewriter) => {
-              typewriter
-                .pauseFor(1000)
-                .typeString("Anushka")
-                .pauseFor(1000)
-                .deleteAll()
-                .typeString('Student')
-                .pauseFor(1000)
-                .deleteAll()
-                .typeString('Developer')
-                .pauseFor(1000)
-                .deleteAll()
-                .start();
-            }}
-          />
+        {/* sliding */}
+        <div className='mx-10 my-4 mt-52'>
+          <div className='text-[120px] font-bold '>
+            anushka
+          </div>
+          <div className='text-4xl'>
+            <div className='slide'>
+              <div><div>data analyst</div></div>
+              <div><div>blockchain developer</div></div>
+              <div><div>full-stack developer</div></div>
+
+            </div>
+          </div>
+          {/* type writer */}
+          {/* <div className='text-4xl'>
+            <Typewriter
+              options={{
+                loop: true,
+                delay: 75,
+                html: true,
+              }}
+              onInit={(typewriter) => {
+                typewriter
+                  .typeString("I'm a developer")
+                  .pauseFor(1200)
+                  .deleteAll()
+                  .typeString("a student too")
+                  .pauseFor(800)
+                  .deleteAll()
+                  .typeString("and I love designing ;)")
+                  .pauseFor(800)
+                  .start();
+              }}
+            />
+          </div> */}
+
         </div>
-        {/* marquee */}
-        
+
       </div>
     </div>
   );
